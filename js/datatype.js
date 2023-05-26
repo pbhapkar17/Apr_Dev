4c52ab467805f5817b28bdb53122db20d25f58c9
pbhapkar17 <poonambhapkar17@gmail.com>
2023-05-26 18:19:05 +0530

    data types

--- js/datatype.js
+++ js/datatype.js
@@ -8,13 +8,13 @@
 // undefined.
 // symbol.
 // null.
-//string
+// string
 // Non-primitive (reference) data type(multi value holder)
 //object
 
 //number data type
 
-var xyz = 300; //number data type
+var xyz = 300 ; //number data type
 var no = 1.236 //number
 
 //typeof() operator used get data type of variable
@@ -25,7 +25,59 @@ console.log(typeof(xyz + 20)); //number
 console.log(typeof(xyz + 20 + 'poonam')); //string 
 console.log(typeof(xyz + 20 + 'poonam' + 20)); //string 
 
-let firstName = "shree";
+//string DT
+let firstName = "shree"; //string
 console.log(typeof(firstName));
 
 var address = "A - 901 , BG CANARY, MUMBAI";
+
+let num2 = "200";
+let res = num2 + 20;
+console.log(res);
+
+//boolean DT
+//true false
+
+let isIndiaBigCountry = true; //boolean
+let isPakistanBigCountry = false;
+
+console.log(typeof(isIndiaBigCountry));
+//let isIndiaBigCountry = 'true';//string
+
+//undefined dt
+let myFavFruit;
+//let myFavFruit = undefined
+console.log(typeof(myFavFruit));
+
+//null dt
+let myCity = null;
+console.log(typeof(myCity)); //object
+
+
+//bigInit
+let cash = 9999999999999999;
+console.log(cash);
+let bigIntVariable = BigInt('99999999999999999616');
+console.log(bigIntVariable);
+
+
+//symbol DT
+let test = Symbol('poonam');
+console.log(typeof(test));
+
+//object DT
+
+let color = ['red','pink','yellow'];//array with 3 values 
+//:hoogeneous array: same data type's items
+let color2 = [];//empty array
+console.log(color);
+console.log(color2);
+
+let items = ['poonam',2020,'9865989899','ctgpp45659p' , test2(), true ];
+
+console.log(items);
+
+
+function test2(){
+    return 'priya';
+}
