7f16458fd737d2589cfc7b060cb5c52086ea06bc
pbhapkar17 <poonambhapkar17@gmail.com>
2023-05-29 20:49:00 +0530

    string interpolation

--- Angular_Dev/src/app/login/login.component.ts
+++ Angular_Dev/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
  formTitle = '---LOGIN FORM-';
- test!: string;
+ test: string ='jbdxbxwndjiendnd jdbijwbdiw2bd';
 
   //propertyName : data type = value ; D+D of property
   //propertyName! : data type; only declareation
@@ -15,12 +16,35 @@ export class LoginComponent {
   amount: number = 50000;
   panNo: any = true;
 
-  submit() {
+  studentName = "Shreeya";
+  studentMarks = 70;
+  isStudentActive = true;
+
+    submit() {
     let num = 20;
     num = 2000;
     //this.test = 'testing property';
     this.formTitle = 'Login Form';
   }
 
+  testFun(){
+    this.submit()
+  }
+
+  interpolationTest(){
+    return 'pune';
+  }
+  formTitleTest(){
+    // let title = '..Login Form..'
+    // return typeof(title) //legel code o/p:string
+    return '..Login Form..'
+  }
+//data binding techniques in angular : data bind
+//1. one way data binding : .ts file's data would get transfer to .hmlt(DOM)
+// 1.String interpolation : {{}}
+// 2.property binding : []
+// 3.Event binding:()
+
+// 2.Two way data bindind[(NgModel)] :[()]
 
 }
