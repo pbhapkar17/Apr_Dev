5fa240615295b092c28d7e3ab5e9e3c6eb116f60
pbhapkar17 <poonambhapkar17@gmail.com>
2023-05-30 18:30:31 +0530

    replace

--- js/stringMethods.js
+++ js/stringMethods.js
@@ -11,7 +11,7 @@ let city = text2.slice(16,22)
 let text3 ='i like-mango';
 //slice(-start index,-end index) //start undex in negative para value will 
 //be includive and end index is exclusive
-console.log(text3.slice(-6,-1));
+console.log(text3.slice(-6));
 console.log(text3.slice(7,12));
 
 //substring() is similar to slice().
@@ -23,3 +23,17 @@ console.log(para.substring(-2,7));
 //The difference is that the second parameter specifies the length of the extracted part.
 let para2 = 'hdf fiqhf kfhiwqhfpi .sncfie';
 console.log(para2.substr(5,10));
+
+//replace()
+//The replace() method replaces only the first match
+//If you want to replace all matches, use a regular expression
+// with the /g flag set. See examples below.
+let para5 = 'pune i like pune';
+let para6 = para5.replace("pune","5000");
+console.log(para6);
+
+let para7 = "pune i like pune";
+//let para8 = para5.replace(/Pune/i,"mumbai");
+let para8 = para5.replace(/pune/g,"mumbai");
+//let para8 = para5.replace(/Pune/ig,"mumbai");
+console.log(para8);
