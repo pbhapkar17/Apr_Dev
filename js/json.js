713834740d7ba7b34fcd349a901beafe1b5e93ce
pbhapkar17 <poonambhapkar17@gmail.com>
2023-06-05 18:13:57 +0530

    json

--- js/json.js
+++ js/json.js
@@ -21,6 +21,7 @@ let emp = {
 
 console.log(emp);
 console.log(emp.employees);
+console.log(emp.employees[0].firstName);
 
 let emp1 =  {
     "employees": [
@@ -34,3 +35,46 @@ let emp1 =  {
 }
 
 console.log(emp1.address);
+
+
+let studentData =
+{
+    studentName: "poonam",
+    studentFriends: ['om', 'Sai', 'Nitish'],
+    mob: 985898999,
+    address: [
+        {
+            colony: "NgCanary",
+            Landmark: "Miraroad",
+            city: "mumbai"
+        }
+    ],
+    marks: [200, 30, [90, 6666, [10, [20, 666]]]],
+    test: [
+        {
+            FruitName: 'Mango'
+        },
+        {
+            FruitName: 'Berry'
+        }
+    ]
+}
+
+console.log(studentData);
+console.log(studentData.studentName);
+console.log(studentData.studentFriends[2]);
+console.log(studentData.mob);
+console.log(studentData.address[0].city);
+console.log(studentData.marks);
+console.log(studentData.marks[2][2][0]);
+console.log(studentData.marks[2][2][1][0]);
+console.log(studentData.test);
+console.log(studentData.test[1].FruitName);
+
+//JSONStringyfy():when data needs to send to the backend 
+//it would get sent in jsonstringyfy format
+console.log(studentData);
+let stringyFiedData = JSON.stringify(studentData);
+console.log(stringyFiedData);
+let parsedDate = JSON.parse(stringyFiedData);
+console.log(parsedDate);
