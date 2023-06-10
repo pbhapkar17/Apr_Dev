da5fba5849266a1b7c6392e455d7ed7b2e59390c
pbhapkar17 <poonambhapkar17@gmail.com>
2023-06-10 22:41:00 +0530

    atteribute

--- ANGULAR_PROJECT/src/app/directives-in-angular/directives-in-angular.component.ts
+++ ANGULAR_PROJECT/src/app/directives-in-angular/directives-in-angular.component.ts
@@ -6,31 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./directives-in-angular.component.css']
 })
 export class DirectivesInAngularComponent {
-    show = false;
-    show1 = true;
-    toShowPara1!:string;
-    toShowPara : string = 'poonam';
-    undefinedProperty : any;
-    nullProperty = null;
-    toggel : boolean = false;
-    cars : string[] = ['mini cup','jaguar','swift','marcedeez'];
-    carDetails = [
-      {carName : 'Swift', carPrice:800000, color:'white'},
-      {carName : 'min cup', carPrice:2500000, color:'white'},
-      {carName : 'marcedeez', carPrice:8000000, color:'white'},
-      {carName : 'i10', carPrice:500000, color:'white'},
-      {carName : 'i20', carPrice:1000000, color:'black'},
-    ];
+  show = false;
+  show1 = true;
+  toShowPara1!: string;
+  toShowPara: string = 'poonam';
+  undefinedProperty: any;
+  nullProperty = null;
+  toggel: boolean = false;
+  cars: string[] = ['mini cup', 'jaguar', 'swift', 'marcedeez'];
+  carDetails = [
+    { carName: 'Swift', carPrice: 800000, color: 'white' },
+    { carName: 'min cup', carPrice: 2500000, color: 'white' },
+    { carName: 'marcedeez', carPrice: 8000000, color: 'white' },
+    { carName: 'i10', carPrice: 500000, color: 'white' },
+    { carName: 'i20', carPrice: 1000000, color: 'black' },
+  ];
+  tableHeadings: string[] = ['CarName', 'CarPrice', 'Color'];
 
-    data = [20,60,50,30,[60,30,89]]
-   
-    toggelEffect(){
-      let abc ;
-      //this.toggel = false;
-      // this.toggel = !true >>false and  !false >>true
-      this.toggel = !this.toggel;
-      
-    }
+  data = [20, 60, 50, 30, [60, 30, 89]];
+  data2:any[] = [];
+  data3:any[] = [];
+  //let xyz = []; //js / within function only
+  // xyz = [];  property with empty array
+  color = 'pink';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.data.forEach((item,i)=>{
+      if( i != 4 ){
+        this.data2.push(item)
+      }
+    })
+
+    this.data.forEach((item:any,i)=>{
+      if( i == 4){
+        this.data3 = [...item]; //
+      }
+    })
+  }
+
+
+  toggelEffect() {
+    let abc;
+    //this.toggel = false;
+    // this.toggel = !true >>false and  !false >>true
+    this.toggel = !this.toggel;
+
+  }
+
+
 
 
 }
