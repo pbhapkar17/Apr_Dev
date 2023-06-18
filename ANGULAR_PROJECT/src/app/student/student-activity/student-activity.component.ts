7f2e1efc944334423418e95645a6f2b9d11b8bf3
pbhapkar17 <poonambhapkar17@gmail.com>
2023-06-18 11:08:42 +0530

    custom validation

--- ANGULAR_PROJECT/src/app/student/student-activity/student-activity.component.ts
+++ ANGULAR_PROJECT/src/app/student/student-activity/student-activity.component.ts
@@ -11,24 +11,40 @@ export class StudentActivityComponent {
   showSignInForm : boolean = false;
   passwordValue : any;
   confirmPassValue : any;
-  isMatch! : boolean
+  isMatch! : boolean;
+  isMatchConfirm!:boolean;
 
   constructor(private fb: FormBuilder){}
 
   showForm(){
-     this.showSignInForm = true;
+     //this.showSignInForm = true;
+     this.showSignInForm = ! this.showSignInForm 
      this.formDef();
   }
 
   formDef(){
 
     this.sigInForm = this.fb.group({
-      password:[],
-      confirmPassword:[]
-     })
+      password:['',[Validators.required]],
+      confirmPassword:[],
+      name:['',[this.nameValidation]]
+     }
+     )
 
   }
 
+
+  nameValidation(inp:any){
+   console.log(inp.value);
+   let data = inp.value?.toLowerCase();
+   let isErr = data.includes('copy')
+    return isErr ? {Err : true}: null;
+  }
+
+
+  passwordMatchingValidatior(){
+
+  }
   password() {
 
     this.passwordValue = this.sigInForm.value.password;
