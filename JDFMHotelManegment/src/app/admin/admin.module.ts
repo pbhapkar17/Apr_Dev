21177c5bf298eb189258927eaeabccd671b79305
pbhapkar17 <poonambhapkar17@gmail.com>
2023-07-23 12:27:24 +0530

    ggg

--- JDFMHotelManegment/src/app/admin/admin.module.ts
+++ JDFMHotelManegment/src/app/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { SharedModule } from '../common/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AdminComponent } from './admin.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    SharedModule
   ]
 })
 export class AdminModule { }
